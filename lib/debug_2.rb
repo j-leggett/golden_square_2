50a233a77e37366987c584423ec7fd85431fe133
Jack <jwleggett@outlook.com>
2023-03-16 15:07:26 +0000

    debug_2 debuged

--- lib/debug_2.rb
+++ lib/debug_2.rb
@@ -1,5 +1,5 @@
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char)).chr
   end
@@ -7,11 +7,18 @@ def encode(plaintext, key)
 end
 
 def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
+    cipher[char.ord - 65]
   end
   return plaintext_chars.join
 end
 
-encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+
+# Intended output:
+#
+p encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
+#
+p decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+# => "theswiftfoxjumpedoverthelazydog"
